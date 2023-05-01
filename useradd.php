7634d75389a90da90ba2428d87f0d0c4689816b8
iFrxs <jrhidalgo.rommel@gmail.com>
2023-05-01 20:21:02 +0800

    arrange  inputs

--- useradd.php
+++ useradd.php
@@ -13,57 +13,146 @@
     include "./imports.php";
     include "./navigation.php";
     ?>
-    <div class="modal-dialog">
-        <div class="modal-content" style="font-size: 14px; font-family: Times New Roman;color:black;">
-            <div class="modal-header" style="background:#222d32">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title" style="font-weight: bold;color: #F0F0F0">
-                    <center>
+    <div class="modal-dialog" style="background-color:green; width:100%">
+            <center>
                         ADD EMPLOYEE DETAILS
-                    </center>
-                </h4>
-            </div>
+            </center>
 
             <div class="modal-body">
                 <center>
-                    <form method="post" action="upload.php" enctype='multipart/form-data' style="width: 98%;">
+                <form method="post" action="upload.php" enctype='multipart/form-data'>
 
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;First Name:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="firstName"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;Middle Name:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="middleName"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">Last Name:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="lastName"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Suffix:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="suffix"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp;Gender:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="gender"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp;Position:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="position"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;Area of Assignment<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="areaOfAssignment"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;Regular/SubAllotment:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="regular_suballotment"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;Contract Duration(start)<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="contractDuration_start"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;Contract Duration (end)<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="contractDuration_end"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;Inclusive Date of Employment:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="inclusiveDateOfEmployment"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;Salary Grade:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="salaryGrade"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;Salary:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="salary"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;PRC ID Number (if application):<label style="color: red;font-size:20px;"></label><input style="width:270px;" type="text" name="prc"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;Address:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="address"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;Birthdate:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="birthdate"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;Place of Birth:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="placeOfBirth"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;Name of Person to Notify in Case of Emergency:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="nameOfPersonToNotify"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;Bloodtype:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="bloodType"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;TIN Number:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="tinNumber"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;PHILHEALTH:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="philhealth"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;SSS:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="sss"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;PAGIBIG Number:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="pagibigNumber"></span></p>
-                        <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;CP Number:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="cpNumber"></span></p>
-                        <p><span style="font-size: 18px; font-weight: bold;">&nbsp;&nbsp; &nbsp;&nbsp;Email Address:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="emailAddress" id='oldpass'></span></p>
-                        Add Signature photo:<input name='sigFiled' type='file' id='sigFiled'>
-                        Add ID photo:<input name='IDFiled' type='file' id='IDFiled'>
+                    <div class="personal_info">
+                        <p>
+                            <span>First Name:<label>*</label></span>
+                            <input type="text" name="firstName" required>
+                        </p>
+                        <p>
+                            <span>Middle Name:<label>*</label></span>
+                            <input type="text" name="middleName" required>
+                        </p>
+                        <p>
+                            <span>Last Name:<label>*</label></span>
+                            <input type="text" name="lastName" required>
+                        </p>
+                        <p>
+                            <span>Suffix:</span>
+                            <input type="text" name="suffix">
+                        </p>
+                        <p>
+                            <span>Gender:<label>*</label></span>
+                            <input type="text" name="gender" required>
+                        </p>
+                        <p>
+                            <span>Birthdate:<label>*</label></span>
+                            <input type="text" name="birthdate" required>
+                        </p>
+                        <p>
+                            <span>Place of Birth:<label>*</label></span>
+                            <input type="text" name="placeOfBirth" required>
+                        </p>
+                        <p>
+                            <span>Address:<label>*</label></span>
+                            <input type="text" name="address" required>
+                        </p>
+                        <p>
+                            <span>Blood Type:<label>*</label></span>
+                            <input type="text" name="bloodType" required>
+                        </p>
+                    </div>
+                    
+                    <div class="contact_info">
+                        <p>
+                            <span>CP Number:<label>*</label></span>
+                            <input type="text" name="cpNumber">
+                        </p>
+                        <p>
+                            <span>Email Address:<label>*</label></span>
+                            <input type="text" name="emailAddress" id='oldpass'>
+                        </p>
+                    </div>
+
+                    <div class="emergency_contact">
+                        <p>
+                            <span>Name of Person to Notify in Case of Emergency:<label>*</label></span>
+                            <input type="text" name="nameOfPersonToNotify" required>
+                        </p>
+                    </div>
+
+                    <div class="govId_Num">
+                        <p>
+                            <span>PRC ID Number (if applicable):</span>
+                            <input type="text" name="prc">
+                        </p>
+                        <p>
+                            <span>TIN Number:<label>*</label></span>
+                            <input type="text" name="tinNumber" required>
+                        </p>
+                        <p>
+                            <span >PHILHEALTH:<label>*</label></span>
+                            <input type="text" name="philhealth">
+                        </p>
+                        <p>
+                            <span>SSS:<label>*</label></span>
+                            <input type="text" name="sss">
+                        </p>
+                        <p>
+                            <span>PAGIBIG Number:<label>*</label></span>
+                            <input type="text" name="pagibigNumber">
+                        </p>        
+                    </div>
+                    
+                    <div class="employment_info">
+                        <p>
+                            <span>Position:<label>*</label></span>
+                            <input type="text" name="position" required>
+                        </p>
+                        <p>
+                            <span>Area of Assignment:<label>*</label></span>
+                            <input type="text" name="areaOfAssignment" required>
+                        </p>
+                        <p>
+                            <span>Regular/SubAllotment:<label>*</label></span>
+                            <input type="text" name="regular_suballotment" required>
+                        </p>
+                        <p>
+                            <span>Contract Duration (start):<label>*</label></span>
+                            <input type="text" name="contractDuration_start" required>
+                        </p>
+                        <p>
+                            <span>Contract Duration (end):<label>*</label></span>
+                            <input type="text" name="contractDuration_end" required>
+                        </p>
+                        <p>
+                            <span>Inclusive Date of Employment:<label>*</label></span>
+                            <input type="text" name="inclusiveDateOfEmployment" required>
+                        </p>
+                        <p>
+                            <span>Salary Grade:<label>*</label></span>
+                            <input type="text" name="salaryGrade" required>
+                        </p>
+                        <p>
+                            <span>Salary:<label>*</label></span>
+                            <input type="text" name="salary" required>
+                        </p>
+                    </div>     
+
+                    <p>
+                        <span>Add Signature photo:</span>
+                        <input name='sigFiled' type='file' id='sigFiled'>
+                    </p>
+                    <p>
+                        <span> Add ID photo:</span>
+                        <input name='IDFiled' type='file' id='IDFiled'>
                         <input type="hidden" name="page" value="admin.php" />
-                </center>
+                    </p>
+                    </center>
+                    <div class="">
+                        <input type="submit" class="btn btn-success" value="Submit" id="addEmployee" name="addEmployee"> 
+                        <button type="button" class="btn btn-success" data-dismiss="modal">Close</button>
+                    </div>
+                </form>
             </div>
-            <div class="modal-footer">
-                <input type="submit" class="btn btn-success" value="Submit" id="addEmployee" name="addEmployee"> &nbsp;
-                <button type="button" class="btn btn-success" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-        </form>
     </div>
 </body>
 
