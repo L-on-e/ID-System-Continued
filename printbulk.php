0c28ae41ab7b0929f8c2a45b627ac03bdf3e1876
meown11 <129470423+meown11@users.noreply.github.com>
2023-05-02 09:38:12 +0800

    ...

--- printbulk.php
+++ printbulk.php
@@ -25,76 +25,92 @@ $startsat = $_SESSION['receiptrange'];
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 
 <head>
-<link rel="stylesheet" href="css/font.css">
 	<title>card</title>
 	<style>
+		  body{
+		  	background:#fff;
+		  }
+#bg {
+  width: 1000px;
+  height: 432px;
+ 
+  margin:60px;
+ 	float: left; 
+ 		
+}
 
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600&display=swap');
+#id {
+  width:312px;
+  height:432px;
+  position:absolute;
+  opacity: 0.88;
+font-family: sans-serif;
 
-		body {
-			background: #fff;
-			font-family: 'Poppins', sans-serif;
+		  	transition: 0.4s;
+		  	background-color: #FFFFFF;
+		  	border-radius: 2%;
+			  
 		}
 
-		#bg {
-			width: 1000px;
-			height: 432px;
+@font-face {
+  font-family: 'Barlow';
+  src: url('bootstrap/fonts/barlow-regular.ttf');
+  font-weight: normal;
+}
 
-			margin: 60px;
-			float: left;
+@font-face {
+  font-family: 'Lora';
+  src: url('bootstrap/fonts/lora-regular.ttf');
+  font-weight: normal;
+}
 
-		}
+#id::before {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: url('./images/bg1.png');
+  background-repeat:repeat-x;
+  background-size: 312px 432px;
+  opacity: 1;
+  z-index: -1;
+  text-align:center;
+  border:1px solid #000;
+ 
+}
+ .container{
+		  	font-size: 12px;
+		  	font-family: sans-serif;
+		    
+		  }
+		 .id-1{
+		  	transition: 0.4s;
+		  	width:312px;
+		  	height:432px;
+		  	background: #FFFFFF;
+		  	font-size: 16px;
+		  	font-family: sans-serif;
+		  	float: left;
+		  	margin:auto;		  	
+		  	margin-left:370px;
+		  	border-radius:2%;
+			border:1px solid #000;
 
-		#id {
-			width: 312px;
-			height: 432px;
-			position: absolute;
-			opacity: 0.88;
-			font-family: sans-serif;
-
-			transition: 0.4s;
-			background-color: #FFFFFF;
-			border-radius: 2%;
-
-		}
-
-		#id::before {
-			content: "";
-			position: absolute;
-			width: 100%;
-			height: 100%;
-			background: url('images/bg1.png');
-			/*if you want to change the background image replace logo.png*/
-			background-repeat: repeat-x;
-			background-size: 312px 432px;
-			opacity: 1;
-			z-index: -1;
-			text-align: center;
-			border: 1px solid #000;
-
-		}
-
-		.container {
-			font-size: 12px;
-			font-family: sans-serif;
-
-		}
-
-		.id-1 {
-			transition: 0.4s;
-			width: 312px;
-			height: 432px;
-			background: #FFFFFF;
-			font-size: 16px;
-			font-family: sans-serif;
-			float: left;
-			margin: auto;
-			margin-left: 370px;
-			border-radius: 2%;
-			border: 1px solid #000;
-
-
-		}
+		  	
+		  }
+		  .vertical-text {
+  writing-mode: vertical-rl;
+  transform: rotate(180deg);
+  position: absolute;
+  left: 5px;
+  bottom: 30px; /* Adjust to desired value */
+   /* Adjust to desired value */
+  font-size: 13px;
+  font-family: 'Barlow';
+  font-weight: bold;
+  white-space: nowrap;
+  color: white;
+}
 	</style>
 </head>
 
@@ -172,33 +188,62 @@ $startsat = $_SESSION['receiptrange'];
 			else 
 				$imageSrc = "admin/images/profile.jpg";
 		}
+		if (filter_var($signature, FILTER_VALIDATE_URL)) {
+			$imageSrc1 = $signature;
+		} else {
+			if ($signature != "")
+				$imageSrc1 = 'images/'. $signature;
+			else 
+				$imageSrc1 = "admin/images/signature.png";
+		}
 	?>
 
 		<div id="bg">
 			<div id="id">
-				<br><br><br><br><br><br><br>
-				<center>
-				<img src="<?= $imageSrc ?>" height="150px" width="160px" alt="image" style="border: 2px solid black;"> </center>
-				<div class="container" align="center">
-
+				<br><br><br><br><br><br><br>			
+				<?php
+             	 	if($imageSrc!=""){          
+						echo "<img src='images/$profilePhoto' height='110px' width='110px' alt='' style='margin-left:20%; margin-top:-3%;'>";
+   
+									    }
+								else{
+									echo"<img src='admin/images/profile.jpg' height='150px' width='160px' alt='' style='border: 2px solid black;'>";	   
+														     	
+									} 
+             	 	 ?>     
+					<div class="container" align="center">
 				<p style="margin-top:-4%">&nbsp;</p>
-					<p style="font-weight: bold;margin-top:-3%;font-size:27px;"><?php if (isset($firstName)) {
-																					$fullName = $firstName . " " . $lastName;
-																					echo $fullName;
-																				} ?></p>
-					<p style="margin-top:-4%">&nbsp;</p>
-					<p style="margin-top:-4%">&nbsp;</p>
-					<p style="margin-top:-4%">&nbsp;</p>
-					<p style="font-weight: bold;margin-top:-4%;font-size:14px;">ID NO:<?php if (isset($id)) {
-																							echo $id;
-																						} ?></p>
-					<p style="font-weight: bold;margin-top:-3%"><?php if (isset($position)) {
-																	echo $position;
-																} ?></p>
-					<p style="font-weight: bold;margin-top:-3%"><?php if (isset($areaOfAssignment)) {
-																	echo $areaOfAssignment;
-																} ?></p>
-					</center>
+	  <p style="margin-top:-4%">&nbsp;</p>
+	  <p style="margin-top:-4%">&nbsp;</p>
+	  <p style="margin-top:-4%">&nbsp;</p>
+	  <p style="margin-top:-4%">&nbsp;</p>
+							
+	  <div style="position: absolute; left: 27%; top: 67%; margin-left:0%; margin-top:-3%; font-size:18px; font-family: 'Lora';">
+	  <span style="font-size:24px;"><?php if(isset($firstName)){echo $firstName;} ?></span></div>
+	  <div style="position: absolute; left: 27%; top: 67%; margin-left:0%; margin-top:-3%; font-size:18px; font-family: 'Lora';">
+  <span><br><?php if(isset($lastName)){echo $lastName;} ?></span>
+  <br>
+
+</div>
+<span style="position: absolute; left: 27%;top: 75%; font-size:9px; font-family: 'Lora';"><?php if(isset($position)){echo $position;} ?></span>
+<p style="margin-top:20%">&nbsp;</p>
+    
+    <p style="position: absolute; top: 0; left: 58%; margin-top:114%; font-size:9px; font-family: 'Lora';"><?php if(isset($id)){ echo$id;} ?></p>
+	<?php
+             	 	if($imageSrc1!=""){          
+						echo "<img src='images/$signature' height='40px' width='140px' alt='' style='margin-left:20%; margin-top:0%;'>";
+   
+									    }
+								else{
+									echo"<img src='admin/images/signature.png' height='150px' width='160px' alt='' style='border: 2px solid black;'>";	   
+														     	
+									} 
+             	 	 ?>  
+		  <p style="margin-top:-4%">&nbsp;</p>
+      	<p style="margin-top:-4%">&nbsp;</p>
+       <p style="margin-top:-4%">&nbsp;</p>
+	   <p class="vertical-text"><?php if(isset($areaOfAssignment)){ echo $areaOfAssignment;} ?></p>
+        
 				</div>
 			</div>
 			<div class="id-1">
@@ -207,17 +252,23 @@ $startsat = $_SESSION['receiptrange'];
 			<div style="margin-top:-5%;border:1px solid #000;margin-left:10px; width: 90%;">
 				<p style="margin-left:10px;"><?php if (isset($address)) {echo $address;} ?></p>
 			</div>
-			<div style="margin-top:1%;border:1px solid #000;margin-left:10px; width: 90%;height:30%;">
-				<p style="text-indent: 30px;text-decoration: underline;">BIRTHDATE</p>
+			<div style="margin-top:1%;border:1px solid #000;margin-left:10px; width: 90%;height:34%;">
+				<p style="margin-top:0%;text-indent: 30px;text-decoration: underline;">BIRTHDATE</p>
 				<p style="margin-top:-12%;text-decoration: underline; text-indent: 160px;">BLOOD TYPE</p>
 				<p style="margin:10px;border:1px solid #000;width: 45%;margin-top:-5%;padding:2px;text-align:center;"><?php if (isset($birthdate)) {echo $birthdate;} ?></p>
 				<p style="margin:180px;border:1px solid #000;width: 20%;margin-top:-12%;padding:2px;text-align:center;"><?php if (isset($bloodtype)) {echo $bloodtype;} ?></p>
 				<p style="text-indent: 35px;text-decoration: underline; margin-top:-60%;">TIN NO.</p>
 				<p style="margin-top:-12%;text-decoration: underline; text-indent: 140px;">PHILHEALTH NO.</p>
+
 				<p style="margin:10px;border:1px solid #000;width: 40%;margin-top:-5%;padding:2px;text-align:center;"><?php if (isset($tinNumber)) {echo $tinNumber;} ?></p>
 				<p style="margin:140px;border:1px solid #000;width: 47%;margin-top:-12%;padding:2px;text-align:center;"><?php if (isset($philhealth)) {echo $philhealth;} ?></p>
+								<p style="margin-top:-50%;text-decoration: underline; text-indent: 123px;">SSS</p>
+				<p style="margin:80px;border:1px solid #000;width: 40%;margin-top:-5%;padding:2px;text-align:center;"><?php if (isset($sss)) {echo $sss;} ?></p>
+
+				
+				
 			</div>
-			<div style="margin-top:2%;border:1px solid #000;margin-left:10px; width: 90%;height:15%;">
+			<div style="margin-top:2%;border:1px solid #000;margin-left:10px; width: 90%;height:18%;">
 				<p style="margin-left:10px;margin-top:1%;">Person to notify incase of emergency:</br>
 					Name: <?php if (isset($nameOfPersonToNotify)) {echo $nameOfPersonToNotify;} ?></br>
 					Tel No: <?php if (isset($cpNumber)) {echo $cpNumber;} ?></p>
@@ -229,7 +280,7 @@ $startsat = $_SESSION['receiptrange'];
 			</hr>
 			<p align="center" style="margin-top:-2%;font-size:12px;">PAULA PAZ M. SYDIONGCO, MD, MPH, MBA, CESO IV <br>Director IV</p>
 
-			</div>
+		</div>
 		</div>
 	<?php } ?>
 
